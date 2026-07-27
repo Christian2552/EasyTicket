e57906922ed4a162e8a628717d69dd1de79785ed
Christian2552 <kriskata2501@gmail.com>
2026-07-27 12:01:38 +0300

    Initial commit

---
+++ MainApp.cs
@@ -0,0 +1,34 @@
+﻿namespace EasyTicket
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            //______________________________________________________________________________
+            // THIS CODE IS FOR TEST PURPOSES
+            // Register User = new Register();
+
+            // User.Authentication_Register();
+
+            // List<string> user = User.info_person;
+
+            // foreach (string i in user)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            // ________________________________________________________________________________
+
+
+            Login User = new Login();
+
+            User.Authentication_Login();
+
+            List<string> user = User.info_person;
+
+            foreach (string i in user)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
